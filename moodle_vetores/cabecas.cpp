0bf07fbf5cb2d5d69ceb608cc3963c05d76cb3d9
victor-pc <victor.aefarias@gmail.com>
2018-05-08 18:23:11 -0300

    Revisao

--- moodle_vetores/cabecas.cpp
+++ moodle_vetores/cabecas.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-using namespace std;
+    #include <iostream>
+    using namespace std;
 
-int preencher_vetor_cabeca(int n, int v[]){
-    for(int i=0; i<n; i++){
-        v[i] = i+1;
-    }
-}
-
-int imprimir_vetor(int n, int v[]){
-    for(int i=0; i<n; i++){
-        cout << v[i] << " ";
-    }
-}
-
-void remover_elemento(int n, int v[], int i){
-    for(int j=i; j<=n-2; j++){
-        v[j] = v[j+1];
-    }
-}
-
-int cabecas(int n, int v[], int x){
-    int i = x-1;
-    while(n > 1){  
-        // Mata o próximo
-        i++;
-        if(i >= n){
-            i =0;
-        }
-        remover_elemento(n, v, i);
-        n--;
-        
-        // Reseta o i
-        if(i>=n){
-            i=0;
+    int preencher_vetor_cabeca(int n, int v[]){
+        for(int i=0; i<n; i++){
+            v[i] = i+1;
         }
     }
-    return v[0];
-}
 
-int main(){
-    int n, x, v[50];
-    cin >> n >> x;
-    preencher_vetor_cabeca(n, v);
-    cout << cabecas(n, v, x);
-    // remover_elemento(n, v, 1);
-    // imprimir_vetor(n-1, v);
-    return 0;
-}
+    int imprimir_vetor(int n, int v[]){
+        for(int i=0; i<n; i++){
+            cout << v[i] << " ";
+        }
+    }
+
+    void remover_elemento(int n, int v[], int i){
+        for(int j=i; j<=n-2; j++){
+            v[j] = v[j+1];
+        }
+    }
+
+    int cabecas(int n, int v[], int x){
+        int i = x-1;
+        while(n > 1){  
+            // Mata o próximo
+            i++;
+            if(i >= n){
+                i =0;
+            }
+            remover_elemento(n, v, i);
+            n--;
+            
+            // Reseta o i
+            if(i>=n){
+                i=0;
+            }
+        }
+        return v[0];
+    }
+
+    int main(){
+        int n, x, v[50];
+        cin >> n >> x;
+        preencher_vetor_cabeca(n, v);
+        cout << cabecas(n, v, x);
+        // remover_elemento(n, v, 1);
+        // imprimir_vetor(n-1, v);
+        return 0;
+    }
